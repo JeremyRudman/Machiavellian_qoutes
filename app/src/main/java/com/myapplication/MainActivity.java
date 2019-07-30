0d3ea8878af315c9462d048bf00a25e12a3918a2
jeremyrudman <jeremyrudman@gmail.com>
2019-07-30 02:16:54 -0400

    this is just a copy of the war hammer qoute app but with diffrent qoutes and icons and other minor diffrences

---
+++ app/src/main/java/com/myapplication/MainActivity.java
@@ -0,0 +1,122 @@
+package com.myapplication;
+
+import android.app.AlarmManager;
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.Color;
+import android.os.Build;
+import android.os.SystemClock;
+import android.support.annotation.RequiresApi;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Random;
+
+public class MainActivity extends AppCompatActivity {
+
+
+
+    @RequiresApi(api = Build.VERSION_CODES.O)
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        final ArrayList<String> qoutes = new ArrayList<>();
+        BufferedReader reader = null;
+        try {
+            reader = new BufferedReader(
+                    new InputStreamReader(getAssets().open("Machiavellian Maxims.txt")));
+            String mLine;
+            while ((mLine = reader.readLine()) != null) {
+                qoutes.add(mLine.substring(2));
+            }
+        } catch (IOException e) {
+
+        } finally {
+            if (reader != null) {
+                try {
+                    reader.close();
+                } catch (IOException e) {
+
+                }
+            }
+        }
+        final TextView qoute = (TextView) findViewById(R.id.qoute);
+        final Random random = new Random();
+        qoute.setText(qoutes.get(random.nextInt(47)));
+        Button button = (Button) findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                qoute.setText(qoutes.get(random.nextInt(47)));
+            }
+
+        });
+
+        final String NOTIFICATION_CHANNEL_ID = "channel_id";
+//User visible Channel Name
+        final String CHANNEL_NAME = "Notification Channel";
+// Importance applicable to all the notifications in this Channel
+        int importance = NotificationManager.IMPORTANCE_DEFAULT;
+//Notification channel should only be created for devices running Android 26
+        NotificationChannel notificationChannel = null;
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            notificationChannel = new NotificationChannel(NOTIFICATION_CHANNEL_ID, CHANNEL_NAME, importance);
+            //Boolean value to set if lights are enabled for Notifications from this Channel
+            notificationChannel.enableLights(true);
+            //Boolean value to set if vibration are enabled for Notifications from this Channel
+            notificationChannel.enableVibration(true);
+            //Sets the color of Notification Light
+            notificationChannel.setLightColor(Color.GREEN);
+            //Set the vibration pattern for notifications. Pattern is in milliseconds with the format {delay,play,sleep,play,sleep...}
+            notificationChannel.setVibrationPattern(new long[]{
+                    500,
+                    300,
+                    500
+            });
+            //Sets whether notifications from these Channel should be visible on Lockscreen or not
+            notificationChannel.setLockscreenVisibility(Notification.VISIBILITY_PUBLIC);
+        }
+        NotificationManager notificationManager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            notificationManager.createNotificationChannel(notificationChannel);
+        }
+
+        startAlarm(true,true);
+
+    }
+
+
+    private void startAlarm(boolean isNotification, boolean isRepeat) {
+        AlarmManager manager = (AlarmManager)getSystemService(Context.ALARM_SERVICE);
+        Intent myIntent;
+        PendingIntent pendingIntent;
+
+        // SET TIME HERE
+        Calendar calendar= Calendar.getInstance();
+        calendar.set(Calendar.HOUR_OF_DAY,13);
+        calendar.set(Calendar.MINUTE,30);
+
+
+        myIntent = new Intent(MainActivity.this,MyReceiver.class);
+        pendingIntent = PendingIntent.getBroadcast(this,0,myIntent,0);
+
+
+        if(!isRepeat)
+            manager.set(AlarmManager.RTC_WAKEUP, SystemClock.elapsedRealtime()+3000,pendingIntent);
+        else
+            manager.setRepeating(AlarmManager.RTC_WAKEUP, calendar.getTimeInMillis(), AlarmManager.INTERVAL_DAY,pendingIntent);
+    }
+}
---
+++ app/src/main/java/com/myapplication/MyReceiver.java
@@ -0,0 +1,70 @@
+package com.myapplication;
+
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.support.v4.app.NotificationCompat;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Random;
+
+import static android.app.PendingIntent.FLAG_ONE_SHOT;
+
+public class MyReceiver extends BroadcastReceiver {
+    public MyReceiver() {
+    }
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(context,"channel_id");
+        final ArrayList<String> qoutes = new ArrayList<>();
+        BufferedReader reader = null;
+        try {
+            reader = new BufferedReader(
+                    new InputStreamReader(context.getAssets().open("Machiavellian Maxims.txt")));
+            String mLine;
+            while ((mLine = reader.readLine()) != null) {
+                qoutes.add(mLine.substring(2));
+            }
+        } catch (IOException e) {
+
+        } finally {
+            if (reader != null) {
+                try {
+                    reader.close();
+                } catch (IOException e) {
+
+                }
+            }
+        }
+        final Random random = new Random();
+
+
+        Intent myIntent = new Intent(context, MainActivity.class);
+        PendingIntent pendingIntent = PendingIntent.getActivity(
+                context,
+                0,
+                myIntent,
+                FLAG_ONE_SHOT );
+
+
+
+        builder.setAutoCancel(true)
+                .setDefaults(Notification.DEFAULT_ALL)
+                .setWhen(System.currentTimeMillis())
+                .setSmallIcon(R.drawable.ic_stat_agenda)
+                .setContentTitle("Warhammer Qoute of The Day")
+                .setContentIntent(pendingIntent)
+                .setContentText(qoutes.get(random.nextInt(47)))
+                .setDefaults(Notification.DEFAULT_LIGHTS | Notification.DEFAULT_SOUND)
+                .setContentInfo("Info");
+        NotificationManager notificationManager = (NotificationManager)context.getSystemService(Context.NOTIFICATION_SERVICE);
+        notificationManager.notify(1,builder.build());
+    }
+}
